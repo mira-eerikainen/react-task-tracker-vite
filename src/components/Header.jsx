f9420dc758c936941a811974f4ace16d5e665b8d
Mira Eerikäinen <miraeerikainen@gmail.com>
2021-10-17 13:19:41 +0300

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router'
+import Button from './button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
+  return (
+    <div>
+      <header className='header'>
+        <h1>{title}</h1>
+        {location.pathname === '/' && (
+          <Button
+            color={showAdd ? '#0748a9' : 'green'}
+            text={showAdd ? 'Close' : 'Add'}
+            onClick={onAdd}
+          />
+        )}
+      </header>
+    </div>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+export default Header
